93fd148c8008b2029a6414a655976d63fe6bfe42
Alex Mason <apmason119@gmail.com>
2021-10-28 11:08:04 -0400

    Button fix

--- Shared/Assets/Views/AssetOverviewView.swift
+++ Shared/Assets/Views/AssetOverviewView.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+private extension AssetView {
+    struct HeightPreferenceKey: PreferenceKey {
+        static let defaultValue: CGFloat = 0
+
+        static func reduce(value: inout CGFloat,
+                           nextValue: () -> CGFloat) {
+            value = max(value, nextValue())
+        }
+    }
+}
+
 struct AssetView: View {
     
     @ObservedObject var asset: OpenSeaAsset
+    
+    @State private var customHeight: CGFloat?
         
     init(asset: OpenSeaAsset) {
         self.asset = asset
@@ -17,25 +30,68 @@ struct AssetView: View {
     }
     
     var body: some View {
-        if let wrapper = asset.imageWrapper {
-            Button.init {
-                print("button tapped")
-            } label: {
-            #if os(macOS)
-                Image(nsImage: wrapper.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            #else
+        Button.init {
+            print("hey")
+        } label: {
+            if let wrapper = asset.imageWrapper {
+                //                Image(uiImage: wrapper.image)
+                //                        .resizable()
+                //                        .scaledToFit()
+                //                Color.clear
+                //                    .aspectRatio(1, contentMode: .fill)
+                //                    .background(
+                //                        Image(uiImage: wrapper.image)
+                //                                .resizable()
+                //                                .scaledToFill()
+                //                    )
+                //ZStack {
                 Image(uiImage: wrapper.image)
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
-            #endif
-            }.buttonStyle(CardButtonStyle())
-
-        } else {
-            Image(systemName: "wifi.slash")
+                    .scaledToFit()
+                    .cornerRadius(5)
+                
+            } else {
+                //ZStack {
+                Image(systemName: "wifi.slash")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                //}
+            }
         }
+        .buttonStyle(DefaultButtonStyle())
+        //.frame(width: 400, height: 400)
         
+
+       // if let wrapper = asset.imageWrapper {
+//            Color.clear
+//                .background(
+//                    Image(uiImage: wrapper.image)
+//                        .resizable()
+//                        .scaledToFit()
+//                )
+//                Button {
+//                    print("button tapped")
+//
+//                } label: {
+//                    #if os(macOS)
+//                    Image(nsImage: wrapper.image)
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                    #else
+//                    Color.clear
+//                        .background(
+//                            Image(uiImage: wrapper.image)
+//                                .resizable()
+//                                .scaledToFill()
+//                        )
+//
+//                    #endif
+//                }
+//                .buttonStyle(CardButtonStyle())
+//        } else {
+//            Color.gray
+//        }
     }
 }
 
@@ -53,7 +109,9 @@ struct AssetOverviewView: View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 50) {
                 ForEach(assets) { asset in
-                    AssetView(asset: asset)
+                    ZStack {
+                        AssetView(asset: asset)
+                    }
                 }
             }
             .padding(20)
